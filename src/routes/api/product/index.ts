298cb6d717eb09411d822a776353c4b693389a18
Rodrigo Limões <rolimoes@gmail.com>
2023-03-08 09:23:58 -0300

    Adding validate dto middleware

--- src/routes/api/product/index.ts
+++ src/routes/api/product/index.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-
+import { validate } from '@common/utils/validate.utils';
 const router = Router();
 
 // controller
 import { productController } from '@src/product';
 
-router.post('/', productController.create);
+// dtos
+import { ProductDto } from '@product/dtos/product.dto';
+
+// validate
+const validateBody = validate('body');
+
+router.post('/', validateBody(ProductDto), productController.create);
 
 export default router;
