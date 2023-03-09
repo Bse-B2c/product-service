96201b947fbf28979f54ad622ac7406f3b75997c
Rodrigo Limões <rolimoes@gmail.com>
2023-03-09 08:56:47 -0300

    Adding findOne function in controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -29,4 +29,20 @@ export class ProductController {
 			next(e);
 		}
 	};
+
+	findOne = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params;
+
+			const response = await this.service.findOne(+id);
+
+			return res.status(HttpStatusCode.CREATED).send({
+				statusCode: HttpStatusCode.CREATED,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
 }
