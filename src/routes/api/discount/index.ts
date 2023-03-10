af096975075082accfed7589cc4fefe8bfab6061
Rodrigo Limões <rolimoes@gmail.com>
2023-03-09 23:19:05 -0300

    Adding new endpoint to findOne, and delete product

--- src/routes/api/discount/index.ts
+++ src/routes/api/discount/index.ts
@@ -7,10 +7,14 @@ import { discountController } from '@src/discount';
 
 // dtos
 import { DiscountDto } from '@discount/dtos/discount.dto';
+import { ParamsDto } from '@common/dtos/params.dto';
 
 // validate
 const validateBody = validate('body');
+const validateParams = validate('params');
 
 router.post('/', validateBody(DiscountDto), discountController.create);
+router.get('/:id', validateParams(ParamsDto), discountController.findOne);
+router.delete('/:id', validateParams(ParamsDto), discountController.delete);
 
 export default router;
