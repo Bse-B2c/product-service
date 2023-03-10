a68050718323e6f4bf1166f1a8e6ef34b2025a48
Rodrigo Limões <rolimoes@gmail.com>
2023-03-09 22:35:12 -0300

    Adding discount entity in database config

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 import { Product } from '@product/entity/product.entity';
+import { Discount } from '@src/discount/entity/discount.entity';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ export const dataSource = new DataSource({
 	username: process.env['TYPEORM_USERNAME'],
 	password: process.env['TYPEORM_PASSWORD'],
 	database: process.env['TYPEORM_DATABASE'],
-	entities: [Product],
+	entities: [Product, Discount],
 	synchronize: true,
 });
 
