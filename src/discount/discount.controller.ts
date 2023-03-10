80ca8c760a8caea13d9423c0dd3f6604e16dc09f
Rodrigo Limões <rolimoes@gmail.com>
2023-03-10 09:31:36 -0300

    Adding find function in controller

--- src/discount/discount.controller.ts
+++ src/discount/discount.controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { DiscountDto } from '@discount/dtos/discount.dto';
 import { HttpStatusCode } from '@bse-b2c/common';
 import { ParamsDto } from '@common/dtos/params.dto';
+import { SearchDto } from '@discount/dtos/search.dto';
 
 export class DiscountController {
 	constructor(private service: DiscountService) {}
@@ -68,6 +69,29 @@ export class DiscountController {
 			} = req;
 
 			const response = await this.service.update(+id, body);
+
+			return res.status(HttpStatusCode.OK).send({
+				statusCode: HttpStatusCode.OK,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	find = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { page, orderBy, sortOrder, limit, ...search } =
+				req.query as unknown as SearchDto;
+
+			const response = await this.service.find({
+				...search,
+				orderBy: orderBy ?? 'name',
+				sortOrder: sortOrder ?? 'ASC',
+				limit: limit || 10,
+				page: page || 0,
+			});
 
 			return res.status(HttpStatusCode.OK).send({
 				statusCode: HttpStatusCode.OK,
