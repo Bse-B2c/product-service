66d561382e90d1b5fa577d73c60b3f74b320b973
Rodrigo Limões <rolimoes@gmail.com>
2023-03-09 23:18:28 -0300

    Adding findOne function in controller; Adding delete function in controller

--- src/discount/discount.controller.ts
+++ src/discount/discount.controller.ts
@@ -2,6 +2,7 @@ import { DiscountService } from '@discount/interfaces/discountService.interface'
 import { NextFunction, Request, Response } from 'express';
 import { DiscountDto } from '@discount/dtos/discount.dto';
 import { HttpStatusCode } from '@bse-b2c/common';
+import { ParamsDto } from '@common/dtos/params.dto';
 
 export class DiscountController {
 	constructor(private service: DiscountService) {}
@@ -19,6 +20,38 @@ export class DiscountController {
 
 			return res.status(HttpStatusCode.CREATED).send({
 				statusCode: HttpStatusCode.CREATED,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	findOne = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params as unknown as ParamsDto;
+
+			const response = await this.service.findOne(id);
+
+			return res.status(HttpStatusCode.OK).send({
+				statusCode: HttpStatusCode.OK,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	delete = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params as unknown as ParamsDto;
+
+			const response = await this.service.delete(id);
+
+			return res.status(HttpStatusCode.OK).send({
+				statusCode: HttpStatusCode.OK,
 				error: null,
 				data: response,
 			});
