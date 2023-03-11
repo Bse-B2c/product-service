11548a5f67340964b721fd51c546630589a5998d
Rodrigo Limões <rolimoes@gmail.com>
2023-03-11 14:28:29 -0300

    Adding new endpoint to findOne specification

--- src/routes/api/specification/index.ts
+++ src/routes/api/specification/index.ts
@@ -7,14 +7,17 @@ import { specificationController } from '@src/specification/';
 
 // dtos
 import { SpecificationDto } from '@specification/dtos/specification.dto';
+import { ParamsDto } from '@common/dtos/params.dto';
 
 // validate
 const validateBody = validate('body');
+const validateParams = validate('params');
 
 router.post(
 	'/',
 	validateBody(SpecificationDto),
 	specificationController.create
 );
+router.get('/:id', validateParams(ParamsDto), specificationController.findOne);
 
 export default router;
