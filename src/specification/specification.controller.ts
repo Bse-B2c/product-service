43bbb5271ca24c242eeb1e1df5d271a65914ae9e
Rodrigo Limões <rolimoes@gmail.com>
2023-03-11 14:28:12 -0300

    Adding findOne function in controller

--- src/specification/specification.controller.ts
+++ src/specification/specification.controller.ts
@@ -2,6 +2,7 @@ import { SpecificationService } from '@specification/interfaces/specificationSer
 import { NextFunction, Request, Response } from 'express';
 import { SpecificationDto } from '@specification/dtos/specification.dto';
 import { HttpStatusCode } from '@bse-b2c/common';
+import { ParamsDto } from '@common/dtos/params.dto';
 
 export class SpecificationController {
 	constructor(private service: SpecificationService) {}
@@ -15,6 +16,22 @@ export class SpecificationController {
 
 			return res.status(HttpStatusCode.CREATED).send({
 				statusCode: HttpStatusCode.CREATED,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
+
+	findOne = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params as unknown as ParamsDto;
+
+			const response = await this.service.findOne(id);
+
+			return res.status(HttpStatusCode.OK).send({
+				statusCode: HttpStatusCode.OK,
 				error: null,
 				data: response,
 			});
