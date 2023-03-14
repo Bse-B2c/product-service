8dd91552de0af4d28c7dafc5f926c06ab8d828cc
Rodrigo Limões <rolimoes@gmail.com>
2023-03-14 09:05:03 -0300

    Adding new endpoint to find product

--- src/routes/api/product/index.ts
+++ src/routes/api/product/index.ts
@@ -8,10 +8,12 @@ import { productController } from '@src/product';
 // dtos
 import { ProductDto } from '@product/dtos/product.dto';
 import { ParamsDto } from '@common/dtos/params.dto';
+import { SearchDto } from '@product/dtos/search.dto';
 
 // validate
 const validateBody = validate('body');
 const validateParams = validate('params');
+const validateQuery = validate('query');
 
 router.post('/', validateBody(ProductDto), productController.create);
 router.get('/:id', validateParams(ParamsDto), productController.findOne);
@@ -22,5 +24,6 @@ router.put(
 	validateBody(ProductDto),
 	productController.update
 );
+router.get('/', validateQuery(SearchDto), productController.find);
 
 export default router;
